73b22c273dfae54645c05d4a4a2b301955e72bfd
DmitryKoryagin <kor.dima97@mail.ru>
2022-12-15 14:44:01 +0300

    create comments for routes

--- routes/api.php
+++ routes/api.php
@@ -22,29 +22,29 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->post('/logout', [AuthController::class, 'logout']);
 
 Route::middleware('guest:api')->controller(AuthController::class)->group(function(){
-    Route::post('/login', 'login');
-    Route::post('/register', 'register');
+    Route::post('/login', 'login'); //Авторизация пользоватея
+    Route::post('/register', 'register'); //Регистрация
 });
 
 Route::middleware('user')->group(function(){
     Route::controller(ProfileController::class)->group(function(){
-        Route::get("/profile", 'getUser');
-        Route::post("/profile/update", 'update');
+        Route::get("/profile", 'getUser'); //Возвращает данные профиля
+        Route::post("/profile/update", 'update'); //Редактирование профиля
     });
 
     Route::controller(BonusController::class)->group(function(){
-        Route::get('/user/bonuses', 'getInfoBonuses');
+        Route::get('/user/bonuses', 'getInfoBonuses'); //Возврашает кол-во активных пользователей и сгорание бонусов
     });
 
     Route::controller(NotificationController::class)->group(function(){
-        Route::get("/notification", 'getUserNotification');
-        Route::post("/notification/read/{id}", "read");
-        Route::get("notification/count", "getCount");
+        Route::get("/notification", 'getUserNotification'); //Возвращает уведомления для пользователя
+        Route::post("/notification/read/{id}", "read"); //Убирает сообщение для пользователя(делает его прочитанным)
+        Route::get("notification/count", "getCount"); //Возвращает кол-во уведомлений
     });
 
     Route::controller(FeedbackController::class)->group(function(){
-        Route::get("feedback", 'getFeedback');
-        Route::post('feedback/create', 'create');
-        Route::post('feedback/{id}/createMessage', 'createMessage');
+        Route::get("feedback", 'getFeedback'); //Возвращает все все обращения пользователя в обратную связь, если делать запрос с профился админа, то просто вернет все обращаения и сообщеня
+        Route::post('feedback/create', 'create'); //Создание обращения в обратную связь
+        Route::post('feedback/{id}/createMessage', 'createMessage'); //Создание сообщения для определенного обращение, id обращения передается в адресной строке
     });
 });
