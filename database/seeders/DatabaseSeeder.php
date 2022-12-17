b5e566f4c06053b2cfaae44b82a86958a7aaa774
DmitryKoryagin <kor.dima97@mail.ru>
2022-12-17 13:40:59 +0300

    create factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Role;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,11 +16,22 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        Role::factory()->create(
+            [
+                'name' => 'admin'
+            ],
+        );
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        Role::factory()->create(
+            [
+                'name' => 'barista'
+            ],
+        );
+
+        Role::factory()->create(
+            [
+                'name' => 'user'
+            ],
+        );
     }
 }
